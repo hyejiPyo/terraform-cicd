0925ae90175e5bd4f7a6bda98c640bbb50566e30
표혜지 <pyohyeji@MacBook-Air.local>
2025-06-03 13:29:15 +0900

    Update variabe

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "aws_region" {
+    default = "ap-northeast-2"
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr"{
+    default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+    default = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+    default = "ap-northeast-2a"
+}
+
+variable "ami_id" {
+    description = "Amazon Linux 2 AMI ID"
+    default = 
+}
+
+variable "instance_type" {
+    default = "t3.medium"
+}
+
+variable "key_name" {
+    description = "EC2 Key Pair Name"
+    default = "cppm-test"
+}
